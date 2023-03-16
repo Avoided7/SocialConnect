751cd2b005126027d506e48b43911a11e411fd38
Максим Савченко <savchenko.m2019@gmail.com>
2023-03-16 15:02:39 +0200

    Добавьте файлы проекта.

---
+++ SocialConnect.Infrastructure/Data/SocialDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SocialConnect.Infrastructure.Data
+{
+    public class SocialDbContext : IdentityDbContext
+    {
+        public SocialDbContext(DbContextOptions<SocialDbContext> options) : base(options)
+        { }
+    }
+}
